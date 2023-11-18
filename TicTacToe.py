4be46c5a96265a38c3401fde54294eba4bcf8154
JeronimoGarp <jeronimo.garcia26502@ucaldas.edu.co>
2023-11-18 10:06:56 -0500

    Se agrego el readme.md y correcciones

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,5 +1,5 @@
+from copy import deepcopy
 from Tablero import tablero
-import random
 
 # Definición de constantes
 PUNTUACION_VICTORIA = 10
@@ -9,20 +9,17 @@ PUNTUACION_SITUACION_PELIGROSA = -5
 PUNTUACION_DERROTA = -10
 
 def hay_ganador(tablero, jugador):
-    for fila in range(3):
-        if tablero[fila][0] == tablero[fila][1] == tablero[fila][2] == jugador:
+    # Verificar filas y columnas
+    for i in range(3):
+        if all(tablero[i][j] == jugador for j in range(3)) or all(tablero[j][i] == jugador for j in range(3)):
             return True
 
-    for columna in range(3):
-        if tablero[0][columna] == tablero[1][columna] == tablero[2][columna] == jugador:
-            return True
-
-    if tablero[0][0] == tablero[1][1] == tablero[2][2] == jugador:
-        return True
-    if tablero[0][2] == tablero[1][1] == tablero[2][0] == jugador:
+    # Verificar diagonales
+    if all(tablero[i][i] == jugador for i in range(3)) or all(tablero[i][2 - i] == jugador for i in range(3)):
         return True
 
     return False
+
 
 def hay_empate(tablero):
     for fila in tablero:
@@ -30,109 +27,82 @@ def hay_empate(tablero):
             return False
     return True
 
-def evaluar_tablero(tablero, jugador):
-    if hay_ganador(tablero, jugador):
-        return PUNTUACION_VICTORIA
-    elif hay_ganador(tablero, 'O' if jugador == 'X' else 'X'):
-        return PUNTUACION_DERROTA
-    else:
+def evaluar_futuro(tablero, jugador, perspectiva):
+    if hay_empate(tablero):
         return PUNTUACION_NEUTRAL
-
-def evaluar_futuro(tablero, jugador):
     if hay_ganador(tablero, jugador):
-        return PUNTUACION_VICTORIA
-    if hay_ganador(tablero, 'O' if jugador == 'X' else 'X'):
-        return PUNTUACION_DERROTA
+        return PUNTUACION_VICTORIA if jugador == perspectiva else PUNTUACION_DERROTA
+    else:
+        mejor_puntuacion = (
+            PUNTUACION_SITUACION_PELIGROSA
+            if jugador == perspectiva
+            else PUNTUACION_SITUACION_VENTAJOSA
+        )
+        for fila in range(3):
+            for columna in range(3):
+                if tablero[fila][columna] == ' ':
+                    tablero[fila][columna] = jugador
+                    puntuacion = evaluar_futuro(deepcopy(tablero), 'O' if jugador == 'X' else 'X', perspectiva)
+                    tablero[fila][columna] = ' '
 
-    mejor_puntuacion = PUNTUACION_SITUACION_PELIGROSA if jugador == 'X' else PUNTUACION_SITUACION_VENTAJOSA
-
-    for fila in range(3):
-        for columna in range(3):
-            if tablero[fila][columna] == ' ':
-                tablero[fila][columna] = jugador
-                puntuacion = evaluar_futuro(tablero, 'O' if jugador == 'X' else 'X')
-                tablero[fila][columna] = ' '
-
-                if jugador == 'X':
-                    mejor_puntuacion = max(mejor_puntuacion, puntuacion)
-                else:
-                    mejor_puntuacion = min(mejor_puntuacion, puntuacion)
+                    if jugador == perspectiva:
+                        mejor_puntuacion = max(mejor_puntuacion, puntuacion)
+                    else:
+                        mejor_puntuacion = min(mejor_puntuacion, puntuacion)
 
     return mejor_puntuacion
 
-def minimax(tablero, profundidad, jugador):
-    if profundidad == 0 or hay_ganador(tablero, 'X') or hay_ganador(tablero, 'O'):
-        return evaluar_tablero(tablero, 'O')
 
-    if jugador == 'O':
-        mejor_valor = float('-inf')
-        for fila in range(3):
-            for columna in range(3):
-                if tablero[fila][columna] == ' ':
-                    tablero[fila][columna] = jugador
-                    valor = minimax(tablero, profundidad - 1, 'X')
-                    tablero[fila][columna] = ' '
-                    mejor_valor = max(mejor_valor, valor)
-        return mejor_valor
-    else:
-        mejor_valor = float('inf')
-        for fila in range(3):
-            for columna in range(3):
-                if tablero[fila][columna] == ' ':
-                    tablero[fila][columna] = jugador
-                    valor = minimax(tablero, profundidad - 1, 'O')
-                    tablero[fila][columna] = ' '
-                    mejor_valor = min(mejor_valor, valor)
-        return mejor_valor
 
-def buscar_mejor_jugada(tablero, jugador):
-    if jugador == 'X':
-        mejor_valor = float('-inf')
-        mejor_jugada = None
 
-        for fila in range(3):
-            for columna in range(3):
-                if tablero[fila][columna] == ' ':
-                    tablero[fila][columna] = jugador
-                    valor = minimax(tablero, 9, 'O')
-                    tablero[fila][columna] = ' '
+def imprimir_tablero(tablero):
+    for fila in tablero:
+        print(" | ".join(fila))
+        print("-" * 9)
 
-                    if valor > mejor_valor:
-                        mejor_valor = valor
-                        mejor_jugada = (fila + 1, columna + 1)
-        return mejor_jugada
-    else:
-        jugadas_disponibles = [(fila + 1, columna + 1) for fila in range(3) for columna in range(3) if tablero[fila][columna] == ' ']
-        return random.choice(jugadas_disponibles)
+def imprimir_jugadas_siguientes(tablero, jugador):
+    print("\nPosibles Jugadas en el Siguiente Movimiento:")
+    
+    for i in range(1, 10):
+        fila = (i - 1) // 3
+        columna = (i - 1) % 3
+
+        if tablero[fila][columna] == ' ':
+            tablero[fila][columna] = jugador
+            puntuacion = evaluar_futuro(deepcopy(tablero), 'O' if jugador == 'X' else 'X', jugador)
+            tablero[fila][columna] = ' '
+
+            if puntuacion == PUNTUACION_VICTORIA:
+                print(f"Jugada: ({fila + 1}, {columna + 1}), Puntuación: 10 (Victoria)")
+            elif puntuacion >= PUNTUACION_SITUACION_VENTAJOSA:
+                print(f"Jugada: ({fila + 1}, {columna + 1}), Puntuación: 5 (Ventajosa)")
+            elif puntuacion == PUNTUACION_NEUTRAL:
+                print(f"Jugada: ({fila + 1}, {columna + 1}), Puntuación: 0 (Neutral)")
+            elif puntuacion <= PUNTUACION_SITUACION_PELIGROSA:
+                print(f"Jugada: ({fila + 1}, {columna + 1}), Puntuación: -5 (Peligrosa)")
+            elif puntuacion == PUNTUACION_DERROTA:
+                print(f"Jugada: ({fila + 1}, {columna + 1}), Puntuación: -10 (Derrota)")
 
 tablero_TicTac = tablero()
 
-print("Tablero Vacío:")
-for fila in tablero_TicTac:
-    print(" | ".join(fila))
-    print("-" * 9)
+print("Tablero Inicial:")
+imprimir_tablero(tablero_TicTac)
 
 jugador = 'X'
 
 while True:
     try:
-        if jugador == 'X':
-            mejor_jugada = buscar_mejor_jugada(tablero_TicTac, jugador)
-            if mejor_jugada is not None:
-                fila, columna = mejor_jugada
-        else:
-            fila = int(input("Introduce la fila (1-3): "))
-            columna = int(input("Introduce la columna (1-3): "))
+        imprimir_jugadas_siguientes(tablero_TicTac, jugador)
+        fila = int(input("Introduce la fila (1-3): "))
+        columna = int(input("Introduce la columna (1-3): "))
 
         if 1 <= fila <= 3 and 1 <= columna <= 3 and tablero_TicTac[fila - 1][columna - 1] == ' ':
             tablero_TicTac[fila - 1][columna - 1] = jugador
-            print("Jugador " + jugador + " ha jugado.")
-            for fila in tablero_TicTac:
-                print(" | ".join(fila))
-                print("-" * 9)
+            print(f"Jugador {jugador} ha jugado:")
+            imprimir_tablero(tablero_TicTac)
 
             if hay_ganador(tablero_TicTac, jugador):
-                print("Jugador " + jugador + " ha ganado.")
+                print(f"Jugador {jugador} ha ganado.")
                 break
             elif hay_empate(tablero_TicTac):
                 print("Empate.")
